8ebefd5e15ebdebd2b74df5c78e183d1dbcace2f
wanglimin <lmwang.nju@gmail.com>
2017-11-28 04:18:46 +0800

    add kinetics model

---
+++ get_kinetics_reference_model.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+wget -O 112_c3d_resnet_18_kinetics.caffemodel http://people.ee.ethz.ch/~limwang/artnet_model/112_c3d_resnet_18_kinetics.caffemodel
+wget -O 112_artnet_resnet_18_kinetics.caffemodel http://people.ee.ethz.ch/~limwang/artnet_model/112_artnet_resnet_18_kinetics.caffemodel
+wget -O 112_artnet_tsn_resnet_18_kinetics.caffemodel http://people.ee.ethz.ch/~limwang/artnet_model/112_artnet_tsn_resnet_18_kinetics.caffemodel
+
+
+
+
+
