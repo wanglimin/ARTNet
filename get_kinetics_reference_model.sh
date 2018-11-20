d597eb6aede7327fa38f387975f4e78192949be5
Limin Wang <lmwang.nju@gmail.com>
2018-11-20 16:42:13 +0800

    Update get_kinetics_reference_model.sh

--- get_kinetics_reference_model.sh
+++ get_kinetics_reference_model.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-wget -O 112_c3d_resnet_18_kinetics.caffemodel http://people.ee.ethz.ch/~limwang/artnet_model/112_c3d_resnet_18_kinetics.caffemodel
-wget -O 112_artnet_resnet_18_kinetics.caffemodel http://people.ee.ethz.ch/~limwang/artnet_model/112_artnet_resnet_18_kinetics.caffemodel
-wget -O 112_artnet_tsn_resnet_18_kinetics.caffemodel http://people.ee.ethz.ch/~limwang/artnet_model/112_artnet_tsn_resnet_18_kinetics.caffemodel
+wget -O 112_c3d_resnet_18_kinetics.caffemodel http://mcg.nju.edu.cn/models/ARTNet/112_c3d_resnet_18_kinetics.caffemodel
+wget -O 112_artnet_resnet_18_kinetics.caffemodel http://mcg.nju.edu.cn/models/ARTNet/112_artnet_resnet_18_kinetics.caffemodel
+wget -O 112_artnet_tsn_resnet_18_kinetics.caffemodel http://mcg.nju.edu.cn/models/ARTNet/112_artnet_tsn_resnet_18_kinetics.caffemodel
 
 
 
